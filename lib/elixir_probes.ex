e6f8ffee3cc7aea8a0f3a799a0b927c2030dac91
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-03-04 13:48:12 +0100

    $ mix new elixir_probes --sup

---
+++ lib/elixir_probes.ex
@@ -0,0 +1,18 @@
+defmodule ElixirProbes do
+  @moduledoc """
+  Documentation for ElixirProbes.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirProbes.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_probes_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirProbesTest do
+  use ExUnit.Case
+  doctest ElixirProbes
+
+  test "greets the world" do
+    assert ElixirProbes.hello() == :world
+  end
+end
